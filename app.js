7ddbb72093cc779e1ac9c699bd5f57a6922f9ccc
thomasoide <thomas.t.oide@gmail.com>
2018-04-23 14:01:12 -0500

    Streamlined model/query file

--- app.js
+++ app.js
@@ -10,7 +10,8 @@ var sessionStore = new session.MemoryStore;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
-app.use(cookieParser('secret'));
+app.use(express.static('views'));
+app.use(cookieParser('keyboard cat'));
 app.use(session({
     cookie: { maxAge: 60000 },
     store: sessionStore,
@@ -18,11 +19,12 @@ app.use(session({
     resave: 'true',
     secret: 'secret'
 }));
+
 app.use(flash());
 app.set("view engine", "ejs");
-app.all('/express-flash', function( req, res ) {
+app.all('/express-flash', function(req, res) {
     req.flash('error', 'Record does not exist');
-    res.redirect(301, '/');
+    res.redirect(301, '/:agency/:search/');
 });
 
 app.get("/", function(req, res){
@@ -53,43 +55,33 @@ app.post("/:agency/searchDept", function(req, res){
     var agency = req.params.agency;
     var dept = req.body.select;
 
-    if (req.params.agency == 'city') {
-      connection = model.createConnection();
+    if(req.params.agency === 'city') {
 
-      connection.connect(function(err){
-        if (err) throw error;
+      model.query('SELECT * FROM como_employees WHERE Department = ?', dept, function (err, results) {
+        if (err) {
+          console.log('Error: ', err);
+        }
+        else if (results.length !== 0){
+          res.render("CityResults", {results: results});
+        }
+        else {
+          res.render("CityNoResult", {results: results});
+        }
       });
-
-      connection.query('SELECT * FROM como_employees WHERE Department = ?', dept, function (err, results) {
-          if (err) throw err;
-          if (results.length !== 0) {
-            res.render("CityResults", {results: results});
-          }
-          else {
-            res.render("CityResults", {results: results});
-          }
-        });
-
-      connection.end();
     }
+
     else if (req.params.agency == 'govt') {
-      connection = model.createConnection();
 
-      connection.connect(function(err){
-        if (err) throw error;
+      model.query('SELECT * FROM salary WHERE agency = ?', dept, function (err, results) {
+        if (err) throw err
+
+        else if (results.length !== 0){
+          res.render("govtResults", {results: results});
+        }
+        else {
+          res.render("govtResults", {results: results});
+        }
       });
-
-      connection.query('SELECT * FROM salary WHERE agency = ?', dept, function (err, results) {
-          if (err) throw err;
-          if (results.length !== 0) {
-            res.render("govtResults", {results: results});
-          }
-          else {
-            res.render("govtResults", {results: results});
-          }
-        });
-
-      connection.end();
     }
 });
 
@@ -101,47 +93,37 @@ app.post("/:agency/searchName", function(req, res){
     var dept = req.body.select;
 
     if(req.params.agency === 'city') {
-    connection = model.createConnection();
 
-    connection.connect(function(err){
-      if (err) throw error;
-    });
-
-    connection.query('SELECT * FROM como_employees WHERE First LIKE ? AND Last LIKE ?', params, function (err, results) {
-      if (err) throw err;
-      if (results.length !== 0) {
+    model.query('SELECT * FROM como_employees WHERE First LIKE ? AND Last LIKE ?', params, function(err, results) {
+      if (err) {
+        console.log('Error: ', err);
+      }
+      else if (results.length !== 0){
         res.render("CityResults", {results: results});
       }
       else {
-        res.render("CityResults", {results: results});
+        res.render("CityNoResult", {results: results});
       }
     });
-    connection.end();
+
     }
 
     else if(req.params.agency === 'govt') {
-      connection = model.createConnection();
 
-      connection.connect(function(err){
-        if (err) throw error;
-      });
-
-      connection.query('SELECT * FROM salary WHERE first LIKE ? AND last LIKE ?', params, function (err, results) {
+      model.query('SELECT * FROM salary WHERE first LIKE ? AND last LIKE ?', params, function(err, results) {
         if (err) throw err;
-        if (results.length !== 0) {
+        else if (results.length !== 0) {
           res.render("govtResults", {results: results});
         }
         else {
           res.render("govtResults", {results: results});
         }
       });
-      connection.end();
     }
-
 });
 
 app.get("*", function(req, res){
-  res.send("Sorry, page not found");
+  res.send("Sorry, page does not exist");
 });
 
 app.listen(3000, () => console.log('Server has started!'));
--- models/model.js
+++ models/model.js
@@ -4,19 +4,31 @@ var mysql = require('mysql');
 
 module.exports = {
 
-  //createsConnection to the database, make sure to input correct DB credentials
-  createConnection: function() {
+  query: function(query, params, callback) {
+
     var connection = mysql.createConnection({
       host     : 'localhost',
       user     : 'root',
       password : 'JZI0K79qe2Xyonw',
       database : 'new_schema'
     });
-    return connection;
+
+    connection.connect(function(err){
+      if (err) throw error;
+    });
+
+    connection.query(query, params, function (err, results) {
+      if (err) throw err;
+      else {
+        callback(null, results);
+      }
+    });
+
+    connection.end();
+
   }
-
-
 }
+
 
 
 // var obj = {};
